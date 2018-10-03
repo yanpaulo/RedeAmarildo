d8c5e5fd9fd8f4145b326610c425f679c0d70bff
Yan Paulo <yanpaulo@hotmail.com>
2018-10-03 20:05:29 -0300

    OpenGL plot.

---
+++ RedeAmarildo.Plot.OpenGL/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace RedeAmarildo.Plot.OpenGL
+{
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Console.WriteLine("Iris: ");
+            Console.WriteLine(Algoritmo.algoritmoIris);
+            Console.WriteLine("-----------------------------\n\n");
+
+            Console.WriteLine("Custom: ");
+            Console.WriteLine(Algoritmo.algoritmoCustom);
+            Console.WriteLine();
+
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
